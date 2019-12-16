fd8cc33219cb45c7dc2357bef1f15b86aebd8934
Andrew Lamansky <andrewlamansky@gmail.com>
2019-12-16 16:48:34 -0500

    revert OrderCopy func to component

--- frontend/components/copy/OrderCopy.js
+++ frontend/components/copy/OrderCopy.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function OrderCopy(order) {
+function OrderCopy(props) {
   return (
     <p>
-      {`Thank you for your order, ${order.customerName}! A confirmation email has
-      been sent to ${order.customerEmail}. You will recieve a follow-up email
-      when the below items are shipped to ${order.customerAddress}.`}
+      {`Thank you for your order, ${props.order.customerName}! A confirmation email has
+      been sent to ${props.order.customerEmail}. You will recieve a follow-up email
+      when the below items are shipped to ${props.order.customerAddress}.`}
     </p>
   );
 }
